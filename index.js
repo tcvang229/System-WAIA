64e61f6b5134858f76d417874e04706d4031082b
tswjfwmeng <44828775+tswjfwmeng@users.noreply.github.com>
2018-11-12 22:46:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,17 +15,18 @@ function initiateCommand(event) {
 
   console.log("initiateCommand function is firing");
 
-  var window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-  SpeechRecognition.lang = "en-US";
-  SpeechRecognition.continuous = false;
-  SpeechRecognition.interimResults = false;
-  SpeechRecognition.onstart = function() {
+  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+  recognition.lang = "en-US";
+  recognition.continuous = false;
+  recognition.interimResults = false;
+  recognition.onstart = function() {
     console.log("initiateCommand has STARTED");
   };
-  SpeechRecognition.onend = function() {
+  recognition.onend = function() {
     console.log("initiateCommand has ENDED");
   }
-  SpeechRecognition.onresult = function(event) {
+  recognition.onresult = function(event) {
     var transcript = Array.from(events.results)
     .map(result => result[0])
     .map(result => result.transcript)
